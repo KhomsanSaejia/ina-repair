5ee2b128cd976df135b772d2be0bda45530d7a70
boss <bosssara@live.com>
2022-01-21 09:44:42 +0700

    change path api

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -89,66 +89,66 @@ class MyConstant {
   // String urlInOutAll = 'http://innoligent1.ddns.net:12345/working/select/all';
   // String urlInOutEdit = 'http://innoligent1.ddns.net:12345/working/select/edit';
 
-  String urlUserAdd = 'https://5ddc-171-97-35-75.ngrok.io/repair/user/add';
-  String urlUserEdit = 'https://5ddc-171-97-35-75.ngrok.io/repair/user/edit';
+  String urlUserAdd = 'https://2f74-171-97-15-55.ngrok.io/repair/user/add';
+  String urlUserEdit = 'https://2f74-171-97-15-55.ngrok.io/repair/user/edit';
   String urlUserDelete =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/user/delete';
+      'https://2f74-171-97-15-55.ngrok.io/repair/user/delete';
   String urlUserCheck =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/user/username';
-  String urlUserRead = 'https://5ddc-171-97-35-75.ngrok.io/repair/user/all';
-  String urlBranchAdd = 'https://5ddc-171-97-35-75.ngrok.io/repair/branch/add';
+      'https://2f74-171-97-15-55.ngrok.io/repair/user/username';
+  String urlUserRead = 'https://2f74-171-97-15-55.ngrok.io/repair/user/all';
+  String urlBranchAdd = 'https://2f74-171-97-15-55.ngrok.io/repair/branch/add';
   String urlBranchEdit =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/branch/edit';
+      'https://2f74-171-97-15-55.ngrok.io/repair/branch/edit';
   String urlBranchDelete =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/branch/delete';
+      'https://2f74-171-97-15-55.ngrok.io/repair/branch/delete';
   String urlBranchCheck =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/branch/check';
-  String urlBranchRead = 'https://5ddc-171-97-35-75.ngrok.io/repair/branch/all';
+      'https://2f74-171-97-15-55.ngrok.io/repair/branch/check';
+  String urlBranchRead = 'https://2f74-171-97-15-55.ngrok.io/repair/branch/all';
   String urlBranchName =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/branch/checkname';
+      'https://2f74-171-97-15-55.ngrok.io/repair/branch/checkname';
   String urlBranchNameCheck =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/branch/name';
-  String urlJobAdd = 'https://5ddc-171-97-35-75.ngrok.io/repair/job/add';
-  String urlJobEdit = 'https://5ddc-171-97-35-75.ngrok.io/repair/job/edit';
-  String urlJobCheck = 'https://5ddc-171-97-35-75.ngrok.io/repair/job/check';
+      'https://2f74-171-97-15-55.ngrok.io/repair/branch/name';
+  String urlJobAdd = 'https://2f74-171-97-15-55.ngrok.io/repair/job/add';
+  String urlJobEdit = 'https://2f74-171-97-15-55.ngrok.io/repair/job/edit';
+  String urlJobCheck = 'https://2f74-171-97-15-55.ngrok.io/repair/job/check';
   String urlStatuscheck =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/status/check';
-  String urlStatuschec2 = 'https://5ddc-171-97-35-75.ngrok.io/re/st/ch/2';
-  String urlStatuschec3 = 'https://5ddc-171-97-35-75.ngrok.io/re/st/ch/3';
-  String urlHistory = 'https://5ddc-171-97-35-75.ngrok.io/repair/history/work';
-  String urlHistory2 = 'https://5ddc-171-97-35-75.ngrok.io/re/hi/wo2';
+      'https://2f74-171-97-15-55.ngrok.io/repair/status/check';
+  String urlStatuschec2 = 'https://2f74-171-97-15-55.ngrok.io/re/st/ch/2';
+  String urlStatuschec3 = 'https://2f74-171-97-15-55.ngrok.io/re/st/ch/3';
+  String urlHistory = 'https://2f74-171-97-15-55.ngrok.io/repair/history/work';
+  String urlHistory2 = 'https://2f74-171-97-15-55.ngrok.io/re/hi/wo2';
   String urlStockCheck =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/stock/check';
-  String urlStockAdd = 'https://5ddc-171-97-35-75.ngrok.io/repair/stock/add';
-  String urlStockEdit = 'https://5ddc-171-97-35-75.ngrok.io/repair/stock/edit';
-  String urlStockTranOut = 'https://5ddc-171-97-35-75.ngrok.io/re/st/tf/out';
+      'https://2f74-171-97-15-55.ngrok.io/repair/stock/check';
+  String urlStockAdd = 'https://2f74-171-97-15-55.ngrok.io/repair/stock/add';
+  String urlStockEdit = 'https://2f74-171-97-15-55.ngrok.io/repair/stock/edit';
+  String urlStockTranOut = 'https://2f74-171-97-15-55.ngrok.io/re/st/tf/out';
   String urlStockTranOutCheck =
-      'https://5ddc-171-97-35-75.ngrok.io/re/st/tf/ch';
+      'https://2f74-171-97-15-55.ngrok.io/re/st/tf/ch';
   String urlStockRec =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/stock/recieve';
+      'https://2f74-171-97-15-55.ngrok.io/repair/stock/recieve';
   String urlStockRecAdd =
-      'https://5ddc-171-97-35-75.ngrok.io/repair/stock/recieve/add';
-  String urlStockTranInCheck = 'https://5ddc-171-97-35-75.ngrok.io/re/st/tr/in';
-  String urlStockTranInAdd = 'https://5ddc-171-97-35-75.ngrok.io/re/st/tr/ad';
-  String urlBranchList = 'https://5ddc-171-97-35-75.ngrok.io/re/br/na';
-  String urlGetItem = 'https://5ddc-171-97-35-75.ngrok.io/item';
-  String urlStockOut = 'https://5ddc-171-97-35-75.ngrok.io/re/st/out';
-  String urlStockOutList = 'https://5ddc-171-97-35-75.ngrok.io/re/st/out/list';
-  String urlStockBillLading = 'https://5ddc-171-97-35-75.ngrok.io/re/st/ld';
+      'https://2f74-171-97-15-55.ngrok.io/repair/stock/recieve/add';
+  String urlStockTranInCheck = 'https://2f74-171-97-15-55.ngrok.io/re/st/tr/in';
+  String urlStockTranInAdd = 'https://2f74-171-97-15-55.ngrok.io/re/st/tr/ad';
+  String urlBranchList = 'https://2f74-171-97-15-55.ngrok.io/re/br/na';
+  String urlGetItem = 'https://2f74-171-97-15-55.ngrok.io/item';
+  String urlStockOut = 'https://2f74-171-97-15-55.ngrok.io/re/st/out';
+  String urlStockOutList = 'https://2f74-171-97-15-55.ngrok.io/re/st/out/list';
+  String urlStockBillLading = 'https://2f74-171-97-15-55.ngrok.io/re/st/ld';
   String urlStockBillLadingEdit =
-      'https://5ddc-171-97-35-75.ngrok.io/re/st/ld/ed';
-  String urlStockPipeAdd = 'https://5ddc-171-97-35-75.ngrok.io/re/st/out/pipe';
+      'https://2f74-171-97-15-55.ngrok.io/re/st/ld/ed';
+  String urlStockPipeAdd = 'https://2f74-171-97-15-55.ngrok.io/re/st/out/pipe';
   String urlStockPipe =
-      'https://5ddc-171-97-35-75.ngrok.io/re/st/out/list/pipe';
+      'https://2f74-171-97-15-55.ngrok.io/re/st/out/list/pipe';
   String urlStockBillLadingCancel =
-      'https://5ddc-171-97-35-75.ngrok.io/re/st/ld/can';
-  String urlPmAdd = 'https://5ddc-171-97-35-75.ngrok.io/pm/add';
-  String urlPm = 'https://5ddc-171-97-35-75.ngrok.io/pm';
-  String urlInOut = 'https://5ddc-171-97-35-75.ngrok.io/working/stamp';
-  String urlInOutDetail = 'https://5ddc-171-97-35-75.ngrok.io/working/select';
-  String urlInOutAll = 'https://5ddc-171-97-35-75.ngrok.io/working/select/all';
+      'https://2f74-171-97-15-55.ngrok.io/re/st/ld/can';
+  String urlPmAdd = 'https://2f74-171-97-15-55.ngrok.io/pm/add';
+  String urlPm = 'https://2f74-171-97-15-55.ngrok.io/pm';
+  String urlInOut = 'https://2f74-171-97-15-55.ngrok.io/working/stamp';
+  String urlInOutDetail = 'https://2f74-171-97-15-55.ngrok.io/working/select';
+  String urlInOutAll = 'https://2f74-171-97-15-55.ngrok.io/working/select/all';
   String urlInOutEdit =
-      'https://5ddc-171-97-35-75.ngrok.io/working/select/edit';
+      'https://2f74-171-97-15-55.ngrok.io/working/select/edit';
 
   MyConstant();
 }
